7d8db865bdd131318d9b3aad60ac3b5a472eadc1
Bret <Burhooo@gmail.com>
2019-01-01 07:06:27 -0600

    student/teacher inherit from user working

--- app/Student.php
+++ app/Student.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends User
 {
+    protected $table = 'users';
+
     public function teacher()
     {
         return $this->hasOne('App\Teacher');
--- app/Teacher.php
+++ app/Teacher.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends User
 {
+    protected $table = 'users';
+
     public function students()
     {
         return $this->hasMany('App\Student');
