ad1edf6c327d26005bc976d97399a657f9cc1b77
Bret <Burhooo@gmail.com>
2019-01-02 06:11:49 -0600

    user/student relations working

--- app/Student.php
+++ app/Student.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Student extends Model
 {
-    //
+
+    protected $guarded = [];
+    public function teacher()
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
 }
