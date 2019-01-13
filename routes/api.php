bee3ddfd88c48f278c13dade5f8114baec43b35a
Bret <Burhooo@gmail.com>
2019-01-13 07:01:23 -0600

    not sure is routes are in correct location

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,9 @@ Route::prefix('auth')->group(function () {
         Route::post('logout', 'AuthController@logout');
     });
 });
+
+Route::group(['middleware' => 'auth:api'], function(){
+    // Users
+    Route::get('users', 'UserController@index')->middleware('isAdmin');
+    Route::get('users/{id}', 'UserController@show')->middleware('isAdminOrSelf');
+});
