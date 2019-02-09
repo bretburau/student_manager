b8530fe554f22252e31ca2ccf3061d756538e3df
Bret <Burhooo@gmail.com>
2019-02-09 06:32:05 -0600

    app.js cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,4 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/StudentList.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 require('./bootstrap');
 import 'es6-promise/auto'
 import VueAuth from '@websanova/vue-auth'
@@ -41,12 +16,9 @@ Vue.router = router
 Vue.use(VueRouter)
 Vue.component('StudentCard', StudentCard)
 
-// Set Vue authentication
 Vue.use(VueAxios, axios)
-// axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api` //didn't work resolved undefined
 Vue.use(VueAuth, auth)
 
-console.log(router)
 const app = new Vue({
     el: '#app',
     router,
